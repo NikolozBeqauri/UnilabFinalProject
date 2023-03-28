621bb08b2b7f44022a876e2638b5a3d6b93f505a
NikolozBekauri <nikoloz.bekauri.2@iliauni.edu.ge>
2023-03-28 23:03:05 +0400

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+const navBurger = document.querySelector(".burger");
+const navLinks = document.querySelector(".header-nav-links");
+/*burger*/
+function navLinksToggle () {
+    navLinks.classList.toggle("header-nav-links-active");
+}
+navBurger.addEventListener("click", navLinksToggle);
