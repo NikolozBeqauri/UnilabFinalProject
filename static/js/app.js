2cfcaf0a6a93e9996e3dab17e4f1ca8d2f3cfab6
NikolozBekauri <nikoloz.bekauri.2@iliauni.edu.ge>
2023-04-04 17:27:47 +0400

    contactPage

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,27 @@
 const navBurger = document.querySelector(".burger");
 const navLinks = document.querySelector(".header-nav-links");
+
+const modalWrapper = document.querySelector(".modal-wrapper");
+const openButton = document.querySelector(".modal-open-button");
+const closeButton = document.querySelector(".modal-close-button");
+
+
 /*burger*/
 function navLinksToggle () {
     navLinks.classList.toggle("header-nav-links-active");
 }
 navBurger.addEventListener("click", navLinksToggle);
 
+/*modal*/
+
+function modalToggle () {
+    modalWrapper.classList.toggle("modal-active")
+}
+
+window.addEventListener('click', (e) => {
+    if(e.target == document.querySelector(".modal-wrapper")) {
+        modalToggle();
+    }
+})
+openButton.addEventListener("click", modalToggle);
+closeButton.addEventListener("click", modalToggle);
